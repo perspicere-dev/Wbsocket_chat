74dacf7bc1d31bcceea866f60518efc6235d1d5c
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-12-06 18:03:02 +0100

    add server

--- client/app.js
+++ client/app.js
@@ -9,9 +9,42 @@ const messageContentInput = document.getElementById('message-content');
 //GLOBAL BINDING 
 let userName;
 
-login = (event) => {
+function login(event) {
   event.preventDefault();
   console.log('asfwf')
+  if(userNameInput.value) {
+      userName = userNameInput.value;
+      loginForm.classList.remove('show');
+      messagesSection.classList.add('show')
+  } else {
+      alert('enter your name')
+  }
 }
 
-loginForm.addEventListener('submit', login());
+const sendMessage = (event) => {
+  event.preventDefault();
+  
+  if(messageContentInput.value){
+      addMessage(userName, messageContentInput.value);
+      messageContentInput.value = '';
+  } else {
+    alert('type smth')
+  }
+}
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
